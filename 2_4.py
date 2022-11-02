3c662ddcdcb8579fecd6e153188b60b251bb032d
Polina-Kochergina <113861019+Polina-Kochergina@users.noreply.github.com>
2022-11-02 12:24:06 +0300

    Update 2_4.py

--- 2_4.py
+++ 2_4.py
@@ -3,22 +3,16 @@ class Dice(object):
     def __init__(self, sides):
         self.sides = sides
         self.x = []
-        # self.r = random.randint(1,self.sides)
-        # self.history.append(self.r)
 
     def roll(self):
         self.r = random.randint(1,self.sides)
         self.x.append(self.r)
-            # x.append(a)
-        # if len(x) >= 5:
-            # x.pop(0)
         return(self.x)
 
     def show_history(self):
         while len(self.x) > 5:
             self.x.pop(0)
         print(self.x)
-
         return(self.x)
         
 
